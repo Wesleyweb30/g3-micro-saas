facc033be7c4a66c214bc12d7e1540b28a885da6
WesleyWeb30 <Wesleyweb3.0@gmail.com>
2026-03-08 17:19:19 -0300

    adionando pagina na dashboard de manutenção e atualizando layout do dashboard

--- src/app/(private)/dashboard/layout.tsx
+++ src/app/(private)/dashboard/layout.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="pt-br">
+        <html lang="en">
             <body>
                 <div className="flex flex-col lg:flex-row min-h-screen">
                     <aside className="w-full lg:w-64 bg-gradient-to-b from-gray-900 to-gray-700 text-white p-4">
                         <nav>
                             <ul className="space-y-4 text-center lg:text-left">
-                                <li className="hover:underline cursor-pointer">🏠 Home</li>
+                                <li className="hover:underline cursor-pointer">🏠 <Link href="/dashboard"> Home</Link></li>
+
+                                <li className="hover:underline cursor-pointer"> <Link href="/dashboard/manutencao">🛠️ Manutenções </Link></li>
+
                                 <li className="hover:underline cursor-pointer">📊 Relatórios</li>
+
                                 <li className="hover:underline cursor-pointer">⚙️ Configurações</li>
                             </ul>
                         </nav>
