98a73947f43aeabc6cd8d9a352c97aa562322e18
WesleyWeb30 <Wesleyweb3.0@gmail.com>
2026-03-08 12:44:09 -0300

    refatora função produttivoGet para simplificar a construção da URL e remove código comentado

--- src/service/produttivo.service.ts
+++ src/service/produttivo.service.ts
@@ -5,10 +5,9 @@ type Params = {
     formId?: number
 }
 
-export async function produttivoGet(endpoint:string, params?: Params) {
-    const { startDate, endDate, userId, formId } = params || {};
-    const range = `${startDate} - ${endDate}`;
-    const url = `${process.env.PRODUTTIVO_BASE_URL}/${endpoint}?order_type=desc&range_time=${encodeURIComponent(range)}&form_fill[form_ids][]=${formId}&form_fill[user_ids][]=${userId ?? ''}&form_fill[is_valid]=true`
+export async function produttivoGet(endpoint: string) {
+    const url = `${process.env.PRODUTTIVO_BASE_URL}/${endpoint}`;
+
     try {
         const res = await fetch(url,
             {
@@ -27,7 +26,7 @@ export async function produttivoGet(endpoint:string, params?: Params) {
         }
 
         const data = await res.json();
-        console.log('Dados do Produttivo buscados com sucesso:', data);
+        // console.log('Dados do Produttivo buscados com sucesso:', data);
         return data;
 
     } catch (error) {
@@ -36,38 +35,9 @@ export async function produttivoGet(endpoint:string, params?: Params) {
     }
 
 }
-
-// export async function getProduttivo(params?: Params) {
-//     const { startDate, endDate, userId, formId } = params || {};
-//     const range = `${startDate} - ${endDate}`;
-//     const url = `${process.env.PRODUTTIVO_BASE_URL}/form_fills?order_type=desc&range_time=${encodeURIComponent(range)}&form_fill[form_ids][]=${formId}&form_fill[user_ids][]=${userId ?? ''}&form_fill[is_valid]=true`
-//     try {
-//         const res = await fetch(url,
-//             {
-//                 headers: {
-//                     'accept': 'application/json',
-//                     'X-Auth-Login': process.env.PRODUTTIVO_LOGIN || '',
-//                     'X-Auth-Register': process.env.PRODUTTIVO_REGISTER || '',
-//                     'X-Auth-Token': process.env.PRODUTTIVO_TOKEN || ''
-//                 },
-//                 next: { revalidate: 60 }
-//             }
-//         );
-
-//         if (!res.ok) {
-//             throw new Error('Erro ao buscar dados do produttivo')
-//         }
-
-//         const data = await res.json();
-//         console.log('Dados do Produttivo buscados com sucesso:', data);
-//         return data;
-
-//     } catch (error) {
-//         console.error('Erro inesperado:', error)
-//         throw error;
-//     }
-
-// }
-
-
+export async function getProduttivoFormFillsManutencao(params?: Params) {
+    const { startDate, endDate, userId, formId } = params || {};
+    const range = `${startDate} - ${endDate}`;
+    return produttivoGet(`form_fills?order_type=desc&range_time=${encodeURIComponent(range)}&form_fill[form_ids][]=${formId}&form_fill[user_ids][]=${userId ?? ''}&form_fill[is_valid]=true`);
+}
 
